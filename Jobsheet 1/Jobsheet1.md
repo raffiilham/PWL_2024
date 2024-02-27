b501a73650beecf486c6c1de34c091b59ab8d644
Raffi Ilham Maulana <raffiilham47@gmail.com>
2024-02-27 10:59:22 +0700

    UpdateJob2

---
+++ Jobsheet 1/Jobsheet1.md
@@ -0,0 +1,49 @@
+## LAPORAN JOBSHEET 2
+## ROUTE, CONTROLLER ,DAN VIEW
+
+#
+
+Raffi Ilham Maulana
+(22) / TI-2F
+
+#
+
+## 2.Routing
+## Basic Routing
+
+1. Routing Hello World <img src="gambar1.png"><br><br>
+2. Routing World<img src="gambar2.png"><br><br>
+3. Routing Selamat Datang<img src="gambar3.png"><br><br>
+4. Routing About<img src="gambar4.png"><br><br>
+
+## Route Parameters
+
+b. Routing /user/{name} <img src=gambar5.png>karena nilai "NamaAnda" diberikan sebagai nilai parameter {name} dalam URL<br><br>
+c. tidak muncul output/ halaman kosong dikarenakan tidak menyertakan nilai untuk parameter {name} <br><br>
+e. <img src =gambar6.png><br><br>
+f. <img src =gambar7.png><br><br>
+
+
+## Optional Parameters
+
+b. <img src = "gambar8.png"> <br><br>
+c. <img src = "gambar9.png"> <br><br>
+e. <img src = "gambar10.png"> <br><br>
+
+## 3.Controller
+## Membuat Controller
+
+e <img src = "gambar11.png"> <br><br>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,92 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// praktikum 1
+Route::get('/hello', function () {
+    return 'Hello World';
+   });
+   
+
+   Route::get('/world', function () {
+    return 'World';
+   });
+   
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
+
+Route::get('/about', function () {
+    return '2241720226 Raffi Ilham Maulana';
+});
+
+
+// praktikum 2
+    Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+
+    Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+    Route::get('/articles/{id}', function ($id) {
+        return "Halaman Artikel dengan ID " . $id;
+    });
+    
+// Optional Parameters
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+
+    Route::middleware(['first', 'second'])->group(function () {
+        Route::get('/', function () {
+        // Uses first & second middleware...
+        });
+       Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+        });
+       });
+       Route::domain('{account}.example.com')->group(function () {
+        Route::get('user/{id}', function ($account, $id) {
+        //
+        });
+       });
+       Route::middleware('auth')->group(function () {
+       Route::get('/user', [UserController::class, 'index']);
+       Route::get('/post', [PostController::class, 'index']);
+       Route::get('/event', [EventController::class, 'index']);
+       });
+       
+       Route::prefix('admin')->group(function () {
+        Route::get('/user', [UserController::class, 'index']);
+        Route::get('/post', [PostController::class, 'index']);
+        Route::get('/event', [EventController::class, 'index']);
+        });
+    
+        Route::redirect('/here', '/there');
+
+        Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+// Membuat Controller
+
+Route::get("/hello", [WelcomeController::class,"hello"]);
+
+
+// Resource Controller
+
+Route::resource('photos', PhotoController::class);
+
+
+
+
